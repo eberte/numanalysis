1f856b4b0cdc4ef09e797553be5a9ffd043cfbed
Preston Piercey <prestonpiercey@casady.org>
2017-04-12 14:08:25 -0500

    Finished 4.5.11, the viete series. Still needs a doc and readme, will finish over weekend.

--- numanalysis.py
+++ numanalysis.py
@@ -1,6 +1,7 @@
 import math
 # pg 166
 
+
 def e(n):
     '''
     Function 4.5.3
@@ -34,6 +35,17 @@ def sqrt(n):
 
 
 def approxPi(n):
+    """
+    Approximates pi using Madhava Sangamagrama's method
+
+    Parameters:
+        n: number of terms to use
+
+    Return value:
+        the approximate value of pi
+
+    useless to run past 30 iterations: floats are not long/accurate enough
+    """
     x = 0.0
     for i in range(n):
         if i % 2 == 1:
@@ -43,4 +55,43 @@ def approxPi(n):
     return sqrt(12) * x
 
 
-print(approxPi(31))
+def nilakantha(terms):
+    """
+    Approximates pi using the nilakantha series with the number of terms
+    input buy user
+
+    Parameters:
+        terms: number of terms to use
+
+    Return value:
+        the approximate value of pi
+
+    *function breaks at 10^-15 and does not return the correct digit
+    """
+    x = 0.0
+    for i in range(terms):
+        if i % 2 == 1:
+            x = x - (4 / ((i * 2 + 2) * (i * 2 + 3) * (i * 2 + 4)))
+        elif i % 2 == 0:
+            x = x + (4 / ((i * 2 + 2) * (i * 2 + 3) * (i * 2 + 4)))
+    return 3 + x
+
+
+def vieteHelp(terms):
+    """
+    Helps the viete function by producing the denominator
+    """
+    if terms == 1:
+        return sqrt(2)
+    elif terms == 0:
+        return 1
+    else:
+        return sqrt(2 + vieteHelp(terms - 1))
+
+
+def viete(terms):
+    x = 1.0
+    for i in range(terms):
+        x = x * 2 / vieteHelp(i)
+
+    return x
